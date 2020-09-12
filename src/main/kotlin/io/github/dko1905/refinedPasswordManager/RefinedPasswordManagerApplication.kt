0e5dabc3d2d02ca05ce3bc0492d5177a160c6126
Daniel Florescu <Daniel.florescu1905@protonmail.ch>
2020-09-12 10:11:52 +0200

    Init

---
+++ src/main/kotlin/io/github/dko1905/refinedPasswordManager/RefinedPasswordManagerApplication.kt
@@ -0,0 +1,11 @@
+package io.github.dko1905.refinedPasswordManager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RefinedPasswordManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<RefinedPasswordManagerApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/dko1905/refinedPasswordManager/RefinedPasswordManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.dko1905.refinedPasswordManager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RefinedPasswordManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
